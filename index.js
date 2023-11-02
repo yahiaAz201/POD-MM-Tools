6f9705c062df88ccebffb60bbed14c2189007e8b
yahia <yahia.bourouni.79@gmail.com>
2023-11-02 19:11:29 +0100

    change port

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-const port = 3001;
+const port = env.process.PORT || 3001;
 
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
