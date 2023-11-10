ccf07d9cdbdb46d2f258c83b456666ff6a81f04c
yahia <yahia.bourouni.79@gmail.com>
2023-11-10 18:01:24 +0100

    changing urls to deploy into heruko

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,54 +10,58 @@ const { instances, loadUsers } = require("../websocket.js");
 const jwt_secret = process.env.JWT_SECRET;
 
 const signin = async (req, res) => {
-  const { email, password } = req.body;
-  let user = await User.findOne({ email: email });
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email: email });
 
-  if (!user || user.password != password)
-    return res
-      .status(401)
-      .json({ success: false, error: "Email or Password Incorrect " });
+    if (!user || user.password != password)
+      return res
+        .status(401)
+        .json({ success: false, error: "Email or Password Incorrect " });
 
-  if (user.banned)
-    return res.status(401).json({
-      success: false,
-      error:
-        "Sorry, but your account has been banned from accessing this service.",
-    });
+    if (user.banned)
+      return res.status(401).json({
+        success: false,
+        error:
+          "Sorry, but your account has been banned from accessing this service.",
+      });
 
-  const isOnline = instances.get({ _id: user._id.toString() });
+    const isOnline = instances.get({ _id: user._id.toString() });
 
-  if (isOnline && isOnline != []) {
-    user.banned = true;
-    await user.save();
-    instances.update({ _id: user._id.toString() }, { banned: true });
-    isOnline["socket"].send(
-      JSON.stringify({ event: "reset_cookies", payload: null })
-    );
-    const admins = instances.get({ role: "admin" }, { multi: true });
-    admins.forEach((admin) => {
-      if (!admin) return;
-      admin["socket"].send(
-        JSON.stringify({
-          event: "banned_users",
-          payload: user._id.toString(),
-        })
+    if (isOnline && isOnline != []) {
+      user.banned = true;
+      await user.save();
+      instances.update({ _id: user._id.toString() }, { banned: true });
+      isOnline["socket"].send(
+        JSON.stringify({ event: "reset_cookies", payload: null })
       );
-    });
-    loadUsers();
-    return res.status(401).json({
-      success: false,
-      error:
-        "Sorry, but your account has been banned from accessing this service. for sharing your account info",
-    });
-  }
+      const admins = instances.get({ role: "admin" }, { multi: true });
+      admins.forEach((admin) => {
+        if (!admin) return;
+        admin["socket"].send(
+          JSON.stringify({
+            event: "banned_users",
+            payload: user._id.toString(),
+          })
+        );
+      });
+      loadUsers();
+      return res.status(401).json({
+        success: false,
+        error:
+          "Sorry, but your account has been banned from accessing this service. for sharing your account info",
+      });
+    }
 
-  user = _.pick(user, "_id", "email", "banned");
-  const token = jwt.sign(user, jwt_secret, { expiresIn: "30d" });
-  res.send({
-    success: true,
-    token,
-  });
+    user = _.pick(user, "_id", "email", "banned");
+    const token = jwt.sign(user, jwt_secret, { expiresIn: "30d" });
+    res.send({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
 };
 
 const signup = async (req, res) => {
@@ -153,53 +157,61 @@ const getMySubscriptions = async (req, res) => {
 };
 
 const loadSubscription = async (req, res) => {
-  const { id } = req.params;
-  const user = req.user;
+  try {
+    const { id } = req.params;
+    const user = req.user;
 
-  if (!id)
-    return res.status(400).send({
-      success: false,
-      error: "bad request format subscription id are required",
+    if (!id)
+      return res.status(400).send({
+        success: false,
+        error: "bad request format subscription id are required",
+      });
+
+    const { subscriptions } = await user.populate({
+      path: "subscriptions",
+      select: "_id name website cookies",
+      transform: (subscription) => {
+        if (user.mySubscriptions.includes(subscription._id.toString())) {
+          return subscription;
+        } else {
+          const { cookies, ...subscriptionWithoutCookies } =
+            subscription.toObject();
+          subscriptionWithoutCookies["cookies"] = null;
+          return subscriptionWithoutCookies;
+        }
+      },
     });
 
-  const { subscriptions } = await user.populate({
-    path: "subscriptions",
-    select: "_id name website cookies",
-    transform: (subscription) => {
-      if (user.mySubscriptions.includes(subscription._id.toString())) {
-        return subscription;
-      } else {
-        const { cookies, ...subscriptionWithoutCookies } =
-          subscription.toObject();
-        subscriptionWithoutCookies["cookies"] = null;
-        return subscriptionWithoutCookies;
-      }
-    },
-  });
+    const [subscription] = subscriptions.filter(
+      (subscription) => subscription._id == id
+    );
 
-  const [subscription] = subscriptions.filter(
-    (subscription) => subscription._id == id
-  );
+    if (!subscription["cookies"])
+      return res.status(405).send({
+        success: false,
+        error: "you are not subscribed to this subscription",
+      });
 
-  if (!subscription["cookies"])
-    return res.status(405).send({
-      success: false,
-      error: "you are not subscribed to this subscription",
+    res.send({
+      success: true,
+      subscription,
     });
-
-  res.send({
-    success: true,
-    subscription,
-  });
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
 };
 
 const listSubscriptions = async (req, res) => {
-  let subscriptions = await Subscriptions.find();
-  subscriptions = subscriptions.map((subscription) => {
-    const { _id, name, website } = subscription;
-    return { _id, name, website };
-  });
-  res.send(subscriptions);
+  try {
+    let subscriptions = await Subscriptions.find();
+    subscriptions = subscriptions.map((subscription) => {
+      const { _id, name, website } = subscription;
+      return { _id, name, website };
+    });
+    res.send(subscriptions);
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
 };
 
 module.exports = {
